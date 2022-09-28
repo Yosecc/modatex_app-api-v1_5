060e58b69a6df5cc6b67a15b6f078df4d4efd97a
yosecc <yosec.cervino@gmail.com>
2022-09-28 15:48:27 -0300

    update adress

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -20,6 +20,64 @@ class AddressController extends Controller
 
         return response()->json($d);
     }
+
+    public function update($adress, Request $request)
+    {
+        $adress = ClientLocal::find($adress);
+
+        if(!$adress){
+            return response()->json(['status'=> false, 'message'=> 'Adress not found'], 401);
+        }
+
+        // $this->validate($request, [
+        //     'CALLE_NAME'    => '',
+        //     'CALLE_NUM'     => '',
+        //     'CALLE_PISO'    => '',
+        //     'CALLE_DTO'     => '',
+        //     'provincia'     => '',
+        //     'ADDRESS_ZIP'   => '',
+        //     'DELIVERY_HOUR' => '',
+        //     'COMMENTS'      => '',
+        // ]);
+        
+        $adress->COUNTRY_NUM   = $request->COUNTRY_NUM;   
+        $adress->STAT_NUM      = $request->STAT_NUM;      
+        $adress->STAT_STR      = $request->STAT_STR;      
+        $adress->LOCALIDAD     = $request->LOCALIDAD;     
+        $adress->CALLE_NAME    = $request->CALLE_NAME;    
+        $adress->CALLE_NUM     = $request->CALLE_NUM;     
+        $adress->CALLE_DTO     = $request->CALLE_DTO;     
+        $adress->CALLE_PISO    = $request->CALLE_PISO;    
+        $adress->ADDRESS_ZIP   = $request->ADDRESS_ZIP;   
+        $adress->AREA_CODE     = $request->AREA_CODE;     
+        $adress->ADDRESS_NAME  = $request->ADDRESS_NAME;  
+        $adress->COMMENTS      = $request->COMMENTS;      
+        $adress->save();
+
+        return response()->json($this->getData($adress));
+    }
+
+    public function create(Request $request)
+    {
+
+        $adress                = new ClientLocal();
+        $adress->COUNTRY_NUM   = $request->COUNTRY_NUM;   
+        $adress->STAT_NUM      = $request->STAT_NUM;      
+        $adress->STAT_STR      = $request->STAT_STR;      
+        $adress->LOCALIDAD     = $request->LOCALIDAD;     
+        $adress->CALLE_NAME    = $request->CALLE_NAME;    
+        $adress->CALLE_NUM     = $request->CALLE_NUM;     
+        $adress->CALLE_DTO     = $request->CALLE_DTO;     
+        $adress->CALLE_PISO    = $request->CALLE_PISO;    
+        $adress->ADDRESS_ZIP   = $request->ADDRESS_ZIP;   
+        $adress->AREA_CODE     = $request->AREA_CODE;     
+        $adress->ADDRESS_NAME  = $request->ADDRESS_NAME;  
+        $adress->COMMENTS      = $request->COMMENTS;      
+        $adress->save();
+
+        return response()->json($this->getData($adress));
+    }
+
     public function getData($data)
     {
         return [
