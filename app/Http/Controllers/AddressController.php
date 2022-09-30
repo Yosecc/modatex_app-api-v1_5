435caf6c537305736abbaaf59284387d088c92e3
yosecc <yosec.cervino@gmail.com>
2022-09-30 09:58:40 -0300

    up

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -39,20 +39,22 @@ class AddressController extends Controller
         //     'DELIVERY_HOUR' => '',
         //     'COMMENTS'      => '',
         // ]);
+
+        $adress->update($request->all());
         
-        $adress->COUNTRY_NUM   = $request->COUNTRY_NUM;   
-        $adress->STAT_NUM      = $request->STAT_NUM;      
-        $adress->STAT_STR      = $request->STAT_STR;      
-        $adress->LOCALIDAD     = $request->LOCALIDAD;     
-        $adress->CALLE_NAME    = $request->CALLE_NAME;    
-        $adress->CALLE_NUM     = $request->CALLE_NUM;     
-        $adress->CALLE_DTO     = $request->CALLE_DTO;     
-        $adress->CALLE_PISO    = $request->CALLE_PISO;    
-        $adress->ADDRESS_ZIP   = $request->ADDRESS_ZIP;   
-        $adress->AREA_CODE     = $request->AREA_CODE;     
-        $adress->ADDRESS_NAME  = $request->ADDRESS_NAME;  
-        $adress->COMMENTS      = $request->COMMENTS;      
-        $adress->save();
+        // $adress->COUNTRY_NUM   = $request->COUNTRY_NUM;   
+        // $adress->STAT_NUM      = $request->STAT_NUM;      
+        // $adress->STAT_STR      = $request->STAT_STR;      
+        // $adress->LOCALIDAD     = $request->LOCALIDAD;     
+        // $adress->CALLE_NAME    = $request->CALLE_NAME;    
+        // $adress->CALLE_NUM     = $request->CALLE_NUM;     
+        // $adress->CALLE_DTO     = $request->CALLE_DTO;     
+        // $adress->CALLE_PISO    = $request->CALLE_PISO;    
+        // $adress->ADDRESS_ZIP   = $request->ADDRESS_ZIP;   
+        // $adress->AREA_CODE     = $request->AREA_CODE;     
+        // $adress->ADDRESS_NAME  = $request->ADDRESS_NAME;  
+        // $adress->COMMENTS      = $request->COMMENTS;      
+        // $adress->save();
 
         return response()->json($this->getData($adress));
     }
