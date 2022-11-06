a79093e6cb966d8cb93855b1388f0a523fd675bb
yosecc <yosec.cervino@gmail.com>
2022-11-05 21:23:09 -0300

    changepassword

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -17,4 +17,33 @@ class ClientController extends Controller
 
       return response()->json($client);
     }
+
+    public function change_password(Request $request)
+    {
+        $this->validate($request, [
+            'oldpass'   => 'required',
+            'newpass'   => 'required',
+            // 'email'  => 'required|email',
+        ]);
+
+        if (Auth::user()->email) {
+            $client = Client::where('client_id', Auth::user()->email)->first();
+
+            if($client){
+              $response = $this->ApiRosa([
+                'client_num' => $client->num, 
+                'newpass'=> $request->newpass,
+                'oldpass'=> $request->oldpass ], 'changepass');
+
+              if($response->status == 200){
+                return response()->json(['status'=> true,'message'=> 'Contrasena cambiada con exito']);
+              }
+            }else{
+              return response()->json(['status'=>false,'message'=>'Email no se encuentra registrado'],422);  
+            }
+
+        }else{
+            return response()->json(['status'=>false,'message'=>'Email no se encuentra registrado'],422);
+        }
+    }
 }
