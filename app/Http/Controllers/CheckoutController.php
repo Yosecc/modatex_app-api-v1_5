3edbf62b2e674e16ea8bb15e396e565da253f337
yosecc <yosec.cervino@gmail.com>
2022-12-06 15:22:41 -0300

    get envios

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -12,6 +12,154 @@ class CheckoutController extends Controller
     private $url = 'https://www.modatex.com.ar/modatexrosa3/?c=';
     private $token;
 
+    private $envios = [[
+                'id'           =>  1,
+                'title'        => 'Envío a sucursal',
+                'precios'      =>  ['Desde $740 a todo el país'],
+                'description'  => 'Seleccionando esta opción te enviamos el pedido a la sucursal próxima que elijas en 4 a 7 días.',
+                'description2' =>  'Esta tienda agregará un costo adicional de $70 por traslado hasta el transporte.',
+                'active'       => false,
+                'color'        => '#239B56',
+                'icon'         => 'res://sucursal',
+                'agregados'    =>[
+                    // [
+                    //     'concepto' => 'Costo de traslado',
+                    //     'value' => 70
+                    // ]
+                ],
+                'method'       => 'post_office',
+                'isFree'       => false
+            ],
+            [
+                'id'           =>  2,
+                'title'        => 'Envío a domicilio',
+                'precios'      =>  ['$380 en CABA','$480 en GBA','Desde $1140 al resto del país'],
+                'description'  => 'Te enviamos el pedido a tu domicilio por moto de 48 a 72 horas, de 8:00hs a 20:00hs y, por Correo Argentino y OCA de 5 a 9 días hábiles.',
+                'description2' =>  'Esta tienda agregará un costo adicional de $70 por traslado hasta el transporte.',
+                'active'       =>  false,
+                'color'        =>  '#CA6F1E',
+                'icon'         =>  'res://enviocasa',
+                'agregados'    => [
+                    // [
+                    //     "concepto"=> 'Costo de traslado',
+                    //     "value"=> 70
+                    // ]
+                ],
+                'method'       => 'home_delivery',
+                'isFree'       =>  false
+            ],
+            [
+                'id'               => 3,
+                'title'            =>'Transporte tradicional',
+                'precios'          => ['$150 por costo de traslado hasta el transporte elegido. Luego pagás el resto en destino.'],
+                'description'      => 'Elegí el transporte que llega a tu ciudad.',
+                'description2'     =>'Esta tienda agregará un costo adicional de $70 por manipulación y embalaje.',
+                'active'           => false,
+                'color'            => '#1976D2',
+                'icon'             => 'res://envio',
+                'agregados'        =>[
+                    // [
+                    //     'concepto' => 'Envío',
+                    //     'value'    => 150
+                    // ],
+                    // [
+                    //     'concepto' => 'Manipulación y embalaje',
+                    //     'value'    => 70
+                    // ],
+                ],
+                'method'           => 'transport',
+                'isFree'           => false
+            ],
+            [
+                'id'               => 4,
+                'title'            =>'INTEGRALPACK',
+                'precios'          => ['$850 por costo de servicio.'],
+                'description'      =>'Envíos a terminal de omnibus en 48 a 72 horas. Buscá si llegamos a tu ciudad!',
+                'description2'     => 'Esta tienda agregará un costo adicional de $70 por traslado hasta el transporte.',
+                'active'           => false,
+                'color'            => '#CDDC39',
+                'icon'             => 'res://integralpack',
+                'agregados'        =>[
+                    // [
+                    //     'concepto' => 'Envío',
+                    //     'value'    => 850
+                    // ],
+                    // [
+                    //     'concepto' => 'Costo de traslado',
+                    //     'value'    => 70
+                    // ],
+                ],
+                'method'           => 'integral_pack',
+                'isFree'           => false
+            ],
+            [
+                'id'           => 5,
+                'title'        =>'Retiro por depósito',
+                'precios'      => [],
+                'description'  =>'Retira la compra en el depósito.',
+                'description2' => 'El horario de atención para el retiro de los paquetes en el depósito de Flores, CABA es de Lunes a Viernes de 8:00hs a 15:00hs.',
+                'active'       => false,
+                'color'        => '#5E35B1',
+                'icon'         => 'res://enviostore',
+                'agregados'    => [],
+                'method'       => 'store_pickup',
+                'isFree'       => false
+            ]];
+
+    public function getEnvios(Request $request)
+    {
+        try {   
+            $this->token = Auth::user()->api_token;
+
+             $response = Http::withHeaders([
+              'x-api-key' => $this->token,
+            ])
+            ->asForm()
+            ->post($this->generateUrl(['controller' => 'Checkout','method' => 'shipping_prices']), 
+                $request->all());
+
+
+            if($response->json()['status'] != 'success'){
+                throw new \Exception("No se encontraron resultados");
+            }
+
+            $envios = $this->envios;
+            $datos = [];
+
+            foreach ($response->json()['data'] as $key => $envio) {
+                $indice = array_search($envio['method'], array_column($envios, 'method'));
+                $data = $envios[$indice];
+
+                $data['isFree'] = $envio['is_free'];
+
+                $precios = [
+                    [
+                        'value' => $envio['extra_charges']['cost'],
+                        'concepto' =>  $envio['extra_charges']['descrip']
+                    ]
+                ];
+
+                if(isset($envio['curr'])){
+                    $precios[] = [
+                        'value' => $envio['curr'],
+                        'concepto' =>  'Envío'
+                    ];
+                }
+
+                $data['agregados'] = $precios;
+
+                $datos[] = $data;
+            }
+
+            array_push($datos, $envios[array_search('store_pickup', array_column($envios, 'method'))]);
+
+            return response()->json($datos);
+
+        } catch (\Exception $e) {
+                return response()->json(['message'=>$e->getMessage()],422);
+        }
+    }
+
     public function editClient(Request $request)
     {
         $this->validate($request, [
