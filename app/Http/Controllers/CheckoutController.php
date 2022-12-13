8aedef683173a30a5cac10936984b5e9bfdc46ea
yosecc <yosec.cervino@gmail.com>
2022-12-13 11:01:26 -0300

    save

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -134,8 +134,8 @@ class CheckoutController extends Controller
 
                 $precios = [
                     [
-                        'value' => isset($envio['extra_charges']) ?? $envio['extra_charges']['cost']:0,
-                        'concepto' =>  isset($envio['extra_charges']) ?? $envio['extra_charges']['descrip']:''
+                        'value' => isset($envio['extra_charges']) ? $envio['extra_charges']['cost']:0,
+                        'concepto' =>  isset($envio['extra_charges']) ? $envio['extra_charges']['descrip']:''
                     ]
                 ];
 
